31c3c10ccab47e73c09d3c271eb495ef1a23c4af
Chester ONeill <coneillcodes@gmail.com>
2020-06-13 10:47:12 -0400

    Just upgrading stuff

--- app.js
+++ app.js
@@ -1,96 +1,115 @@
-var nodeHint = require('./node-hint').hint;
-var MongoClient = require('mongodb').MongoClient;
-var format = require('util').format;
-var ObjectID = require('mongodb').ObjectID;
-var fs = require('fs');
-var defaultReporter = require('./node-hint/lib/report').report;
+const fs = require("fs");
+const MongoClient = require("mongodb").MongoClient;
+const nodeHint = require("./node-hint").hint;
+const defaultReporter = require("./node-hint/lib/report").report;
 
-MongoClient.connect('mongodb://admin:ase123@127.0.0.1:27017/zerionProd?authSource=admin', function(err, db) {
-	var collection = db.collection("page");
-	
-  	collection.find({"PAGE.PROFILE": 145087}).toArray(function(err, results) {
-        
-        var str = "";
-	
-		for(var i = 0; i < results.length; i++) {
-			
-			var doc = results[i];
+const report = {
+  reporter: defaultReporter,
+  options: { oneErrorPerLine: false },
+};
 
-			if(doc.PAGE.PAGE_JAVASCRIPT && typeof doc.PAGE.PAGE_JAVASCRIPT === "string") {
-				console.log(doc.PAGE.ID);
-				var options = {
-					source : doc.PAGE.PAGE_JAVASCRIPT,
-					sourceName : "Page Id " + doc.PAGE.ID,
-					report: { reporter: defaultReporter, options: { oneErrorPerLine: false } }
-				}
-		   
-				nodeHint(options, function(err, result){
+MongoClient.connect(
+  "mongodb://admin:ase123@127.0.0.1:27017/zerionProd?authSource=admin",
+  (err, db) => {
+    const collection = db.collection("page");
 
-					str += "PAGE JAVASCRIPT PROBLEMS FOR " + doc.PAGE.NAME + "\n"
-					str += result + "\n";
-					str += "/----------------------------------------------------------------------------------------------------------/\n\n";
-					console.log(result);
-					console.log ();
-				});
-			}
-			
-			for(var j = 0; j < doc.ELEMENTS.length; j++) {
-				
-				var element = doc.ELEMENTS[j];
-				
-				if(element.DYNAMIC_VALUE && typeof element.DYNAMIC_VALUE === "string") {
-					var options = {
-						source : element.DYNAMIC_VALUE,
-						sourceName : "Page Id " + doc.PAGE.ID + " " + element.NAME + " Dynamic Value",
-						report: { reporter: defaultReporter, options: { oneErrorPerLine: false } }
-					}
-		   			
-					nodeHint(options, function(err, result){
-						str += "ELEMENT DYNAMIC_VALUE PROBLEMS FOR " + element.NAME + " IN PAGE " + doc.PAGE.NAME + "\n";
-						str += "JavaScript: " + element.DYNAMIC_VALUE + "\n";
-						str += result + "\n";
-						str += "/----------------------------------------------------------------------------------------------------------/\n\n";
-						console.log(result);
-						console.log ();
-					});
-				}
-				
-				if(element.CONDITION_VALUE && typeof element.CONDITION_VALUE === "string"){
-					var options = {
-						source : element.CONDITION_VALUE,
-						sourceName : "Page Id " + doc.PAGE.ID + " "  + element.NAME + " Condition Value",
-						report: { reporter: defaultReporter, options: { oneErrorPerLine: false } }
-					}
-		   			
-					nodeHint(options, function(err, result){
-						str += "ELEMENT CONDITION_VALUE PROBLEMS FOR " + element.NAME + " IN PAGE " + doc.PAGE.NAME + "\n";
-						str += "JavaScript: " + element.CONDITION_VALUE + "\n";
-						str += result + "\n";
-						str += "/----------------------------------------------------------------------------------------------------------/\n\n";
-						console.log(result);
-						console.log ();
-					});
-				}
+    collection.find({ "PAGE.PROFILE": 145087 }).toArray((err, results) => {
+      let str = "";
 
-				if(element.DYNAMIC_LABEL && typeof element.DYNAMIC_LABEL === "string") { 
-					var options = {
-						source : element.DYNAMIC_LABEL,
-						sourceName : "Page Id " + doc.PAGE.ID + " " + element.NAME + " Dynamic Label",
-						report: { reporter: defaultReporter, options: { oneErrorPerLine: false } }
-					}
-		   			
-					nodeHint(options, function(err, result){
-						str += "ELEMENT DYNAMIC_LABEL PROBLEMS FOR " + element.NAME + " IN PAGE " + doc.PAGE.NAME + "\n";
-						str += "JavaScript: " + element.DYNAMIC_LABEL + "\n";
-						str += result + "\n";
-						str += "/----------------------------------------------------------------------------------------------------------/\n\n";
-						console.log(result);
-						console.log ();
-					});
-				}
-			}
-		}
+      results.forEach((doc) => {
+        if (
+          doc.PAGE.PAGE_JAVASCRIPT &&
+          typeof doc.PAGE.PAGE_JAVASCRIPT === "string"
+        ) {
+          console.log(doc.PAGE.ID);
+          const options = {
+            source: doc.PAGE.PAGE_JAVASCRIPT,
+            sourceName: "Page Id " + doc.PAGE.ID,
+            report,
+          };
 
-		fs.appendFile('CapeFearProd.txt', str, function (err) {});
-	});
-});
+          nodeHint(options, (err, result) => {
+            str += [
+              `PAGE JAVASCRIPT PROBLEMS FOR ${doc.PAGE.NAME}`,
+              result,
+              "/----------------------------------------------------------------------------------------------------------/\n",
+            ].join("\n");
+            console.log(result);
+            console.log();
+          });
+        }
+
+        doc.ELEMENTS.forEach((element) => {
+          if (
+            element.DYNAMIC_VALUE &&
+            typeof element.DYNAMIC_VALUE === "string"
+          ) {
+            const options = {
+              source: element.DYNAMIC_VALUE,
+              sourceName: `Page Id ${doc.PAGE.ID} ${element.NAME} Dynamic Value`,
+              report,
+            };
+
+            nodeHint(options, (err, result) => {
+              str += [
+                `ELEMENT DYNAMIC_VALUE PROBLEMS FOR ${element.NAME} IN PAGE ${doc.PAGE.NAME}`,
+                `JavaScript: + ${element.DYNAMIC_VALUE}`,
+                result,
+                "/----------------------------------------------------------------------------------------------------------/\n",
+              ].join("\n");
+              console.log(result);
+              console.log();
+            });
+          }
+
+          if (
+            element.CONDITION_VALUE &&
+            typeof element.CONDITION_VALUE === "string"
+          ) {
+            const options = {
+              source: element.CONDITION_VALUE,
+              sourceName: `Page Id ${doc.PAGE.ID} ${element.NAME} Condition Value`,
+              report,
+            };
+
+            nodeHint(options, (err, result) => {
+              str += [
+                `ELEMENT CONDITION_VALUE PROBLEMS FOR ${element.NAME} IN PAGE ${doc.PAGE.NAME}`,
+                `JavaScript: ${element.CONDITION_VALUE}`,
+                result,
+
+                "/----------------------------------------------------------------------------------------------------------/\n",
+              ].join("\n");
+              console.log(result);
+              console.log();
+            });
+          }
+
+          if (
+            element.DYNAMIC_LABEL &&
+            typeof element.DYNAMIC_LABEL === "string"
+          ) {
+            const options = {
+              source: element.DYNAMIC_LABEL,
+              sourceName: `Page Id ${doc.PAGE.ID} ${element.NAME} Dynamic Label`,
+              report,
+            };
+
+            nodeHint(options, (err, result) => {
+              str += [
+                `ELEMENT DYNAMIC_LABEL PROBLEMS FOR ${element.NAME} IN PAGE ${doc.PAGE.NAME}`,
+                `JavaScript: ${element.DYNAMIC_LABEL}`,
+                result,
+                "/----------------------------------------------------------------------------------------------------------/\n",
+              ].join("\n");
+              console.log(result);
+              console.log();
+            });
+          }
+        });
+      });
+
+      fs.appendFile("output.txt", str, (err) => {});
+    });
+  }
+);
